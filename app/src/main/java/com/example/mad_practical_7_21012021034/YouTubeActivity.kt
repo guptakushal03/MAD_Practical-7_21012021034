f8895cd8c7c4f62023ac4d0611fcba5fc834729c
Student <student@gnu.ac.in>
2023-09-11 10:30:43 +0530

    Practical 7

---
+++ app/src/main/java/com/example/mad_practical_7_21012021034/YouTubeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical_7_21012021034
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YouTubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_you_tube)
+    }
+}
